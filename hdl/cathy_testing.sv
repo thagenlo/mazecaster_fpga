394ee6c8fdc2815ca43f003bf2443d2e573c7ac4
cathyhu3 <138086864+cathyhu3@users.noreply.github.com>
2024-11-25 10:27:18 -0500

    created testing files + popcat.mem

--- hdl/cathy_testing.sv
+++ hdl/cathy_testing.sv
@@ -45,233 +45,234 @@ module cathy_testing(
     logic last_screen_pixel;
     logic [5:0] frame_count; //0 to 59 then rollover frame counter
 
+
     //CONTROL BUTTONS
 
-    //debouncing buttons
+    // //debouncing buttons
 
-    logic leftRot_btn;
-    logic rightRot_btn;
-    logic fwd_btn;
-    logic bwd_btn;
+    // logic leftRot_btn;
+    // logic rightRot_btn;
+    // logic fwd_btn;
+    // logic bwd_btn;
 
-    // assign leftRot_btn = btn[1];
-    // assign rightRot_btn = btn[0];
-    // assign fwd_btn = btn[3];
-    // assign bwd_btn = btn[2];
+    // // assign leftRot_btn = btn[1];
+    // // assign rightRot_btn = btn[0];
+    // // assign fwd_btn = btn[3];
+    // // assign bwd_btn = btn[2];
  
-    debouncer deb_leftRot_btn (
-        .clk_in(clk_pixel),
-        .rst_in(sys_rst),
-        .dirty_in(btn[1]),
-        .clean_out(leftRot_btn));
+    // debouncer deb_leftRot_btn (
+    //     .clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
+    //     .dirty_in(btn[1]),
+    //     .clean_out(leftRot_btn));
 
-    debouncer deb_rightRot_btn (
-        .clk_in(clk_pixel),
-        .rst_in(sys_rst),
-        .dirty_in(btn[0]),
-        .clean_out(rightRot_btn));
+    // debouncer deb_rightRot_btn (
+    //     .clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
+    //     .dirty_in(btn[0]),
+    //     .clean_out(rightRot_btn));
     
-    debouncer deb_fwd_btn (
-        .clk_in(clk_pixel),
-        .rst_in(sys_rst),
-        .dirty_in(btn[3]),
-        .clean_out(fwd_btn));
+    // debouncer deb_fwd_btn (
+    //     .clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
+    //     .dirty_in(btn[3]),
+    //     .clean_out(fwd_btn));
 
-    debouncer deb_bwd_btn (
-        .clk_in(clk_pixel),
-        .rst_in(sys_rst),
-        .dirty_in(btn[2]),
-        .clean_out(bwd_btn));
+    // debouncer deb_bwd_btn (
+    //     .clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
+    //     .dirty_in(btn[2]),
+    //     .clean_out(bwd_btn));
 
-    //TODO: PIPELINING
+    // //TODO: PIPELINING
 
-    // VIDEO SIGN GEN
-    video_sig_gen mvg(
-        .pixel_clk_in(clk_pixel),
-        .rst_in(sys_rst),
-        .hcount_out(hcount_video),
-        .vcount_out(vcount_video),
-        .vs_out(vert_sync),
-        .hs_out(hor_sync),
-        .ad_out(active_draw),
-        .nf_out(new_frame),
-        .very_last_pixel_out(last_screen_pixel),
-        .fc_out(frame_count));
+    // // VIDEO SIGN GEN
+    // video_sig_gen mvg(
+    //     .pixel_clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
+    //     .hcount_out(hcount_video),
+    //     .vcount_out(vcount_video),
+    //     .vs_out(vert_sync),
+    //     .hs_out(hor_sync),
+    //     .ad_out(active_draw),
+    //     .nf_out(new_frame),
+    //     .very_last_pixel_out(last_screen_pixel),
+    //     .fc_out(frame_count));
 
-    //TODO: INSERT CONTROLLER MODULE
+    // //TODO: INSERT CONTROLLER MODULE
 
-    logic [15:0] posX, posY;
-    logic [15:0] dirX, dirY;
-    logic [15:0] planeX, planeY;
+    // logic [15:0] posX, posY;
+    // logic [15:0] dirX, dirY;
+    // logic [15:0] planeX, planeY;
 
-    controller controller_in (
-        .pixel_clk_in(clk_pixel),
-        .rst_in(sys_rst),
-        .moveFwd(fwd_btn),
-        .moveBack(bwd_btn),
-        .rotLeft(leftRot_btn),
-        .rotRight(rightRot_btn),
-        .valid_in(1),
-        .posX(posX),
-        .posY(posY),
-        .dirX(dirX),
-        .dirY(dirY),
-        .planeX(planeX), 
-        .planeY(planeY),
-        .valid_out(1)
-    );
+    // controller controller_in (
+    //     .pixel_clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
+    //     .moveFwd(fwd_btn),
+    //     .moveBack(bwd_btn),
+    //     .rotLeft(leftRot_btn),
+    //     .rotRight(rightRot_btn),
+    //     .valid_in(1),
+    //     .posX(posX),
+    //     .posY(posY),
+    //     .dirX(dirX),
+    //     .dirY(dirY),
+    //     .planeX(planeX), 
+    //     .planeY(planeY),
+    //     .valid_out(1)
+    // );
 
-    //TODO: INSERT RAY CALCULATION MODULE
+    // //TODO: INSERT RAY CALCULATION MODULE
 
-    //TODO: sending in 320 hcounts
+    // //TODO: sending in 320 hcounts
 
-    logic [8:0] hcount_in_ray;
-    logic [8:0] hcount_ray;
-    logic stepX;
-    logic stepY;
-    logic signed [15:0] rayDirX;
-    logic signed [15:0] rayDirY;
-    logic [15:0] sideDistX;
-    logic [15:0] sideDistY;
-    logic [15:0] deltaDistX;
-    logic [15:0] deltaDistY;
+    // logic [8:0] hcount_in_ray;
+    // logic [8:0] hcount_ray;
+    // logic stepX;
+    // logic stepY;
+    // logic signed [15:0] rayDirX;
+    // logic signed [15:0] rayDirY;
+    // logic [15:0] sideDistX;
+    // logic [15:0] sideDistY;
+    // logic [15:0] deltaDistX;
+    // logic [15:0] deltaDistY;
 
-    //generate all hcounts
-    always_ff @(posedge clk_pixel) begin
-        if (sys_rst) begin
-        hcount_in_ray <= 0;
-        end else begin
-            hcount_in_ray <= hcount_in_ray + 1;
-        end
-    end
-
-
-    logic dda_data_valid_in, dda_data_ready_out;
-
-    ray_calculations calculating_ray (
-        .pixel_clk_in(clk_pixel),
-        .rst_in(sys_rst),
-        .hcount_in(hcount_in_ray),
-        .posX(posX),
-        .posY(posY),
-        .dirX(dirX),
-        .dirY(dirY),
-        .planeX(planeX), 
-        .planeY(planeY),
-        .stepX(stepX),
-        .stepY(stepY),
-        .rayDirX(rayDirX),
-        .rayDirY(rayDirY),
-        .sideDistX(sideDistX),
-        .sideDistY(sideDistY),
-        .deltaDistX(deltaDistX),
-        .deltaDistY(deltaDistY),
-        .hcount_out(hcount_ray),
-        .valid_out(dda_data_valid_in),
-        .dda_data_ready_out(dda_data_ready_out)
-    );
+    // //generate all hcounts
+    // always_ff @(posedge clk_pixel) begin
+    //     if (sys_rst) begin
+    //     hcount_in_ray <= 0;
+    //     end else begin
+    //         hcount_in_ray <= hcount_in_ray + 1;
+    //     end
+    // end
 
 
-    //TODO: INSERT DDA-in FIFO
-    logic dda_fsm_in_tvalid, dda_fsm_in_tready;
-    logic [143:0] dda_fsm_in_tdata;//[138:0] dda_fsm_in_tdata;
+    // logic dda_data_valid_in, dda_data_ready_out;
 
-    dda_fifo_wrap #(
-        .DEPTH(256), //2^8 = 256 - ~320
-        .DATA_WIDTH(144), // 139 bits (8*18 = 144)
-        .PROGFULL_DEPTH(12)
-    )dda_fifo_in ( // read data output from traffic
-        // reset and clock signals
-        .sender_rst(sys_rst),
-        .sender_clk(clk_pixel),
-        .receiver_clk(clk_pixel),
-        // sender interface (input to FIFO)
-        .sender_axis_tvalid(dda_data_valid_in), // Heba input
-        .sender_axis_tready(dda_data_ready_out), // FIFO
-        .sender_axis_tdata({5'b0_0000, hcount_ray, stepX, stepY, rayDirX, rayDirY, deltaDistX, deltaDistY, posX, posY, sideDistX, sideDistY}), // Heba input
-        .sender_axis_tlast(),
-        .sender_axis_prog_full(),
-        // receiver interface (output from FIFO)
-        .receiver_axis_tvalid(dda_fsm_in_tvalid), // out - indicates the FIFO has valid data for the receiver to consume
-        .receiver_axis_tready(dda_fsm_in_tready), // in - indicates the receiver is ready to consume data
-        .receiver_axis_tdata(dda_fsm_in_tdata), //  out - the actual data being received from the FIFO
-        .receiver_axis_tlast(), // FIFO
-        .receiver_axis_prog_empty());
+    // ray_calculations calculating_ray (
+    //     .pixel_clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
+    //     .hcount_in(hcount_in_ray),
+    //     .posX(posX),
+    //     .posY(posY),
+    //     .dirX(dirX),
+    //     .dirY(dirY),
+    //     .planeX(planeX), 
+    //     .planeY(planeY),
+    //     .stepX(stepX),
+    //     .stepY(stepY),
+    //     .rayDirX(rayDirX),
+    //     .rayDirY(rayDirY),
+    //     .sideDistX(sideDistX),
+    //     .sideDistY(sideDistY),
+    //     .deltaDistX(deltaDistX),
+    //     .deltaDistY(deltaDistY),
+    //     .hcount_out(hcount_ray),
+    //     .valid_out(dda_data_valid_in),
+    //     .dda_data_ready_out(dda_data_ready_out)
+    // );
 
-    // DDA MODULE
-    dda #(
-        .SCREEN_WIDTH(320),
-        .SCREEN_HEIGHT(240), 
-        .N(24)
-    ) dda_module (
-        .pixel_clk_in(clk_pixel),
-        .rst_in(sys_rst),
+
+    // //TODO: INSERT DDA-in FIFO
+    // logic dda_fsm_in_tvalid, dda_fsm_in_tready;
+    // logic [143:0] dda_fsm_in_tdata;//[138:0] dda_fsm_in_tdata;
+
+    // dda_fifo_wrap #(
+    //     .DEPTH(256), //2^8 = 256 - ~320
+    //     .DATA_WIDTH(144), // 139 bits (8*18 = 144)
+    //     .PROGFULL_DEPTH(12)
+    // )dda_fifo_in ( // read data output from traffic
+    //     // reset and clock signals
+    //     .sender_rst(sys_rst),
+    //     .sender_clk(clk_pixel),
+    //     .receiver_clk(clk_pixel),
+    //     // sender interface (input to FIFO)
+    //     .sender_axis_tvalid(dda_data_valid_in), // Heba input
+    //     .sender_axis_tready(dda_data_ready_out), // FIFO
+    //     .sender_axis_tdata({5'b0_0000, hcount_ray, stepX, stepY, rayDirX, rayDirY, deltaDistX, deltaDistY, posX, posY, sideDistX, sideDistY}), // Heba input
+    //     .sender_axis_tlast(),
+    //     .sender_axis_prog_full(),
+    //     // receiver interface (output from FIFO)
+    //     .receiver_axis_tvalid(dda_fsm_in_tvalid), // out - indicates the FIFO has valid data for the receiver to consume
+    //     .receiver_axis_tready(dda_fsm_in_tready), // in - indicates the receiver is ready to consume data
+    //     .receiver_axis_tdata(dda_fsm_in_tdata), //  out - the actual data being received from the FIFO
+    //     .receiver_axis_tlast(), // FIFO
+    //     .receiver_axis_prog_empty());
+
+    // // DDA MODULE
+    // dda #(
+    //     .SCREEN_WIDTH(320),
+    //     .SCREEN_HEIGHT(240), 
+    //     .N(24)
+    // ) dda_module (
+    //     .pixel_clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
         
-        // DDA-in FIFO receiver
-        .dda_fsm_in_tvalid(dda_fsm_in_tvalid),
-        .dda_fsm_in_tdata(dda_fsm_in_tdata[138:0]),
-        .dda_fsm_in_tready(dda_fsm_in_tready),
+    //     // DDA-in FIFO receiver
+    //     .dda_fsm_in_tvalid(dda_fsm_in_tvalid),
+    //     .dda_fsm_in_tdata(dda_fsm_in_tdata[138:0]),
+    //     .dda_fsm_in_tready(dda_fsm_in_tready),
         
-        // DDA-out FIFO sender
-        .dda_fsm_out_tready(dda_fsm_out_tready),
-        .dda_fsm_out_tdata(dda_fsm_out_tdata),
-        .dda_fsm_out_tvalid(dda_fsm_out_tvalid),
-        .dda_fsm_out_tlast(dda_fsm_out_tlast)
-    );
+    //     // DDA-out FIFO sender
+    //     .dda_fsm_out_tready(dda_fsm_out_tready),
+    //     .dda_fsm_out_tdata(dda_fsm_out_tdata),
+    //     .dda_fsm_out_tvalid(dda_fsm_out_tvalid),
+    //     .dda_fsm_out_tlast(dda_fsm_out_tlast)
+    // );
 
 
-    //TODO: INSERT DDA-out FIFO
-    //receiver
-    logic dda_fsm_out_tready, dda_fsm_out_tvalid, dda_fsm_out_tlast;
-    logic [37:0] dda_fsm_out_tdata;
-    // fifo-out signal to transformer
-    logic fifo_tvalid_out;
-    logic [39:0] fifo_tdata_out;
-    logic fifo_tlast_out;
-    logic fifo_prog_empty;
-    // transformer signal to fifo-out
-    logic transformer_tready;
+    // //TODO: INSERT DDA-out FIFO
+    // //receiver
+    // logic dda_fsm_out_tready, dda_fsm_out_tvalid, dda_fsm_out_tlast;
+    // logic [37:0] dda_fsm_out_tdata;
+    // // fifo-out signal to transformer
+    // logic fifo_tvalid_out;
+    // logic [39:0] fifo_tdata_out;
+    // logic fifo_tlast_out;
+    // logic fifo_prog_empty;
+    // // transformer signal to fifo-out
+    // logic transformer_tready;
 
-    dda_fifo_wrap #(
-        .DEPTH(256), //2^8 = 256 - ~320
-        .DATA_WIDTH(40), // *multiple of 8 9 (hcount) + 8 (line height) + 1 (wall type) + 4 (map data) + 16 (wallX) = 38 bits = [37:0]
-        .PROGFULL_DEPTH(12)
-    ) dda_fifo_out ( // read data output from traffic
-        .sender_rst(sys_rst),
-        .sender_clk(clk_pixel),
-        .sender_axis_tvalid(dda_fsm_out_tvalid), // in - data on the sender_axis_tdata signal is valid and can be written into the FIFO
-        .sender_axis_tready(dda_fsm_out_tready), // out - FIFO is ready to accept data from the sender
-        .sender_axis_tdata({2'b00, dda_fsm_out_tdata}), // in - actual data being written into the FIFO
-        .sender_axis_tlast(dda_fsm_out_tlast), // in - last piece of data in a frame or packet being sent to the FIFO
-        .sender_axis_prog_full(), //TODO: to here ???
-        .receiver_clk(clk_pixel), //TODO: Replace names starting from here
-        .receiver_axis_tvalid(fifo_tvalid_out),
-        .receiver_axis_tready(transformer_tready),
-        .receiver_axis_tdata(fifo_tdata_out),
-        .receiver_axis_tlast(fifo_tlast_out),
-        .receiver_axis_prog_empty()); // unused
+    // dda_fifo_wrap #(
+    //     .DEPTH(256), //2^8 = 256 - ~320
+    //     .DATA_WIDTH(40), // *multiple of 8 9 (hcount) + 8 (line height) + 1 (wall type) + 4 (map data) + 16 (wallX) = 38 bits = [37:0]
+    //     .PROGFULL_DEPTH(12)
+    // ) dda_fifo_out ( // read data output from traffic
+    //     .sender_rst(sys_rst),
+    //     .sender_clk(clk_pixel),
+    //     .sender_axis_tvalid(dda_fsm_out_tvalid), // in - data on the sender_axis_tdata signal is valid and can be written into the FIFO
+    //     .sender_axis_tready(dda_fsm_out_tready), // out - FIFO is ready to accept data from the sender
+    //     .sender_axis_tdata({2'b00, dda_fsm_out_tdata}), // in - actual data being written into the FIFO
+    //     .sender_axis_tlast(dda_fsm_out_tlast), // in - last piece of data in a frame or packet being sent to the FIFO
+    //     .sender_axis_prog_full(), //TODO: to here ???
+    //     .receiver_clk(clk_pixel), //TODO: Replace names starting from here
+    //     .receiver_axis_tvalid(fifo_tvalid_out),
+    //     .receiver_axis_tready(transformer_tready),
+    //     .receiver_axis_tdata(fifo_tdata_out),
+    //     .receiver_axis_tlast(fifo_tlast_out),
+    //     .receiver_axis_prog_empty()); // unused
 
-    //TODO: INSERT TRANSFORMATION MODULE
-    logic [15:0] ray_address_out;
-    logic [15:0] ray_pixel_out;
-    logic ray_last_pixel_out;
+    // //TODO: INSERT TRANSFORMATION MODULE
+    // logic [15:0] ray_address_out;
+    // logic [15:0] ray_pixel_out;
+    // logic ray_last_pixel_out;
 
-    transformation flattening_module (
-        .pixel_clk_in(clk_pixel),
-        .rst_in(sys_rst),
-        .dda_fifo_tvalid_in(fifo_tvalid_out),
-        .dda_fifo_tdata_in(fifo_tdata_out[37:0]),
-        .dda_fifo_tlast_in(fifo_tlast_out),
-        .transformer_tready_out(transformer_tready),
-        .ray_address_out(ray_address_out),
-        .ray_pixel_out(ray_pixel_out),
-        .ray_last_pixel_out(ray_last_pixel_out)
-    );
+    // transformation flattening_module (
+    //     .pixel_clk_in(clk_pixel),
+    //     .rst_in(sys_rst),
+    //     .dda_fifo_tvalid_in(fifo_tvalid_out),
+    //     .dda_fifo_tdata_in(fifo_tdata_out[37:0]),
+    //     .dda_fifo_tlast_in(fifo_tlast_out),
+    //     .transformer_tready_out(transformer_tready),
+    //     .ray_address_out(ray_address_out),
+    //     .ray_pixel_out(ray_pixel_out),
+    //     .ray_last_pixel_out(ray_last_pixel_out)
+    // );
 
-    // PIXEL VALUE WRITING
+    // // PIXEL VALUE WRITING
     logic [23:0] rgb_out; // from the frame buffer
 
-    frame_buffer frame_buffer_module(
+    frame_buffer_testing frame_buffer_testing_module (
         .pixel_clk_in(clk_pixel),
         .rst_in(sys_rst),
         .hcount_in(hcount_video),
